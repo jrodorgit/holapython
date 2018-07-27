df3eb408e408d5e365e2edffb6b037368f0d5032
jrodorgit <32450968+jrodorgit@users.noreply.github.com>
2018-07-27 11:55:00 +0200

    Create intro.py

---
+++ src-python/intro.py
@@ -0,0 +1,11 @@
+# esto es un comentario.
+
+#esto es una asignacion multiple de variables
+ a,b = 1,2
+ 
+#esto imprime una cadena
+print('c:\hola\numero')
+ 
+# la anterior cadena lleva \n que lo intrepreta como un salto de linea
+# si lo quiero evitar pongo un r delante de la cadena
+print(r'c:\hola\numero')
